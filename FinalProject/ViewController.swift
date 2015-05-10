7ab000c3f0cef58723f73ac0656709b5faa153a5
Sophie Li <zingyli@dhcp-clara-2519.eduroam.cornell.edu>
2015-05-10 16:17:50 -0400

    Initial Commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Sophie Li on 5/10/15.
+//  Copyright (c) 2015 Sophie Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
